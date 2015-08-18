a5e91c3476d63fdfc2b4747634658fe1557ce412
SteelDragony <blaweetniet@live.nl>
2015-08-18 13:12:54 +0200

    First Combat functionality
    health damage and aiming at a predetermined target

--- Assets/Scripts/UnitFire.cs
+++ Assets/Scripts/UnitFire.cs
@@ -5,14 +5,50 @@ public class UnitFire : MonoBehaviour {
 
 	public Transform target;
 
+	public float timeBetweenShots = 0.30f;
+
+	float timer;
+	float effectDisplayTime = 0.10f;
+	LineRenderer gunLine;
+	Light gunLight;
+	public Transform barrelEnd;
+
 	// Use this for initialization
 	void Start () {
-	
+		gunLine = GetComponentInChildren<LineRenderer>();
+		gunLight = GetComponentInChildren<Light>();
+		//barrelEnd = transform.FindChild("GunMuzzle").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Quaternion RotateQuat = Quaternion.FromToRotation(Transform.position, target.position)
-;
+		Vector3 toTarget = target.position - transform.position;
+		toTarget.y = 0f;
+		Quaternion RotateQuat = Quaternion.LookRotation(toTarget);
+		transform.rotation = RotateQuat;
+
+		timer += Time.deltaTime;
+		if(Input.GetButton("Fire1") && timer >= timeBetweenShots)
+		{
+			Shoot();
+		}
+		if(timer >= timeBetweenShots * effectDisplayTime)
+		{
+			DisableEffects();
+		}
+	}
+
+	void DisableEffects()
+	{
+		gunLine.enabled = false;
+		gunLight.enabled = false;
+	}
+	void Shoot()
+	{
+		timer = 0f;
+		gunLight.enabled = true;
+		gunLine.enabled = true;
+		gunLine.SetPosition(0, barrelEnd.position);
+		gunLine.SetPosition(1, target.position);
 	}
 }
---
+++ Assets/Scripts/UnitFire1.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitFire1 : MonoBehaviour {
+
+	public Transform target;
+
+	public float timeBetweenShots = 0.30f;
+
+	float timer;
+	float effectDisplayTime = 0.10f;
+	LineRenderer gunLine;
+	Light gunLight;
+	public Transform barrelEnd;
+	public GameObject turret;
+	// Use this for initialization
+	void Start () {
+		gunLine = GetComponentInChildren<LineRenderer>();
+		gunLight = GetComponentInChildren<Light>();
+		barrelEnd = transform.FindChild("Turret").FindChild("Gunall").FindChild("GunMuzzle");
+		turret = transform.FindChild("Turret").gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(target != null)
+		{
+			Vector3 toTarget = target.position - transform.position;
+			toTarget.y = 0f;
+			Quaternion RotateQuat = Quaternion.LookRotation(toTarget);
+			turret.transform.rotation = RotateQuat;
+			if(Input.GetButton("Fire1") && timer >= timeBetweenShots)
+			{
+				Shoot();
+			}
+		}
+
+		if(timer >= timeBetweenShots * effectDisplayTime)
+		{
+			DisableEffects();
+		}
+	}
+
+	void DisableEffects()
+	{
+		gunLine.enabled = false;
+		gunLight.enabled = false;
+	}
+	void Shoot()
+	{
+		timer = 0f;
+		gunLight.enabled = true;
+		gunLine.enabled = true;
+		gunLine.SetPosition(0, barrelEnd.transform.position);
+		gunLine.SetPosition(1, target.position);
+		UnitHealth targetHealth = target.GetComponent<UnitHealth>();
+		targetHealth.health -= 20;
+	}
+}
